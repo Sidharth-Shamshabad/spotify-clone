db1fb0f6fccd05001d86fd66271aa65ee0071f28
Sidharth Shamshabad <sid@techvoe.com>
2020-10-23 00:04:45 -0400

    completed footer layout and header layout

--- src/Footer.js
+++ src/Footer.js
@@ -5,16 +5,24 @@ import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
 import SkipNextIcon from "@material-ui/icons/SkipNext";
 import ShuffleIcon from "@material-ui/icons/Shuffle";
 import RepeatIcon from "@material-ui/icons/Repeat";
-// import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
 // import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
-// import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
-// import { Grid, Slider } from "@material-ui/core";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import { Grid, Slider } from "@material-ui/core";
 
 function Footer() {
     return (
         <div className="footer">
             <div className="footer__left">
-                <p>Album and song details</p>
+                <img
+                    className="footer__albumLogo" 
+                    src="" 
+                    alt="" 
+                />
+                <div className="footer__songInfo">
+                    <h4>Yeah!</h4>
+                    <p>Usher</p>
+                </div>
             </div>
             <div className="footer__center">
                 <ShuffleIcon className="footer__green" />
@@ -24,7 +32,15 @@ function Footer() {
                 <RepeatIcon className="footer__green" />
             </div>
             <div className="footer__right">
-                <p>Volume controls</p>
+                <Grid item>
+                    <PlaylistPlayIcon />
+                </Grid>
+                <Grid item>
+                    <VolumeDownIcon />
+                </Grid>
+                <Grid item xs>
+                    <Slider />
+                </Grid>
             </div>
         </div>
     )
